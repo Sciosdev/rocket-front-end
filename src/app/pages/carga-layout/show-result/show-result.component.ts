3b457f73de912a00c12ef73e844e11f98c9b60d6
Raul Eduardo Martinez Chavez <raul.zevahc@gmail.com>
2021-06-15 18:15:13 -0500

    Se refresca el componente stepper al finalizar

--- src/app/pages/carga-layout/show-result/show-result.component.ts
+++ src/app/pages/carga-layout/show-result/show-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { Router } from '@angular/router';
 import { NbStepperComponent } from '@nebular/theme';
 
 @Component({
@@ -11,13 +12,14 @@ export class ShowResultComponent implements OnInit, OnChanges {
   @Input() resultado: any;
   @Input() errores: any[];
 
-  constructor(private stepper: NbStepperComponent) { }
+  constructor(private stepper: NbStepperComponent, private router: Router) { }
 
   ngOnInit(): void {
   }
   
   finaliza(){
     this.stepper.reset();
+ //   this.router.navigate(['/intranet/carga-layout']);
   }
   
   ngOnChanges(): void {
