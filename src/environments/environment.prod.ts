de422e8601331330374c9587eca9814e4ae8d29a
Sciosdev <github@scios.club>
2025-06-24 23:33:08 -0600

    Initial commit: Angular project for Rocket Courier

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endpoint: "https://rocketcourierback.com/rocket-back-end",
+  endpoint: "http://18.118.189.189:8080/rocket-back-end",
   clientId: 'rocketApp',
   clientSecret: 'mE8_vt2852d_f@B'
 };
